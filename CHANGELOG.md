a264801aadc6786f271e37e35fc89b6ae35a2e1c
Ziang <ziangting@163.com>
2019-04-17 17:26:54 +0800

    增加自定义字段，更改配置

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,4 @@
-## 0.0.3 (2019-04-11)
-* Move up contest section in README
 
-## 0.0.2 (2019-04-11)
-* Add QR Code for voting
 
-## 0.0.1 (2019-03-24)
+## 0.0.1 (2019-04-17)
 * Initial Release
--- src/reminderView.ts
+++ src/reminderView.ts
@@ -1,6 +1,7 @@
 'use strict';
 import * as vscode from 'vscode';
-import * as path from 'path';
+// import * as path from 'path';
+import { Utility } from './utility';
 
 export class ReminderView {
     private static panel: vscode.WebviewPanel | undefined;
@@ -9,24 +10,29 @@ export class ReminderView {
         if (this.panel) {
             this.panel.reveal();
         } else {
-            this.panel = vscode.window.createWebviewPanel("ycy", "杨超越", vscode.ViewColumn.Two, {
+            this.panel = vscode.window.createWebviewPanel("ycy", "MAYDAY", vscode.ViewColumn.Two, {
                 enableScripts: true,
                 retainContextWhenHidden: true,
             });
 
-            const imagePath = vscode.Uri.file(path.join(context.extensionPath, 'images', 'ycy0.png'))
-                .with({ scheme: 'vscode-resource' });
-
+            // const imagePath = vscode.Uri.file(path.join(context.extensionPath, 'images', 'ycy0.png'))
+            //     .with({ scheme: 'vscode-resource' });
+            const name=Utility.getConfiguration().get<string>('nickName')
+            const imgUrls=Utility.getConfiguration().get<(string)[]>('photos')
+            const len=imgUrls&&imgUrls.length||0
+            const index= Math.floor(Math.random()*(len))
+            const imgUrl= imgUrls&&imgUrls[index]
+            console.log(imgUrls,index)
             this.panel.webview.html = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>杨超越</title>
+    <title>MAYDAY</title>
 </head>
 <body>
-    <div><h1>小哥哥~ 代码写久了，该休息啦~</h1></div>
-    <div><img src="${imagePath}"></div>
+    <div><h1>${name}~ 代码写久了，该休息啦~</h1></div>
+    <div><img src="${imgUrl}"></div>
 </body>
 </html>`;
 
